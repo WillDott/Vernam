522f7674b1125fce192c7cadc84b68d182c45d5b
WillDott <WillDott@users.noreply.github.com>
2017-10-16 16:37:37 +0100

    Main Files

---
+++ Verman/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Verman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.tboxEncrypt = new System.Windows.Forms.RichTextBox();
+            this.tboxKey = new System.Windows.Forms.RichTextBox();
+            this.tboxOut = new System.Windows.Forms.RichTextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(188, 106);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(33, 7);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(102, 13);
+            this.lblInput.TabIndex = 4;
+            this.lblInput.Text = "Enter text to encrypt";
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(33, 168);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(100, 13);
+            this.lblKey.TabIndex = 5;
+            this.lblKey.Text = "Encryption keyword";
+            // 
+            // tboxEncrypt
+            // 
+            this.tboxEncrypt.Location = new System.Drawing.Point(11, 23);
+            this.tboxEncrypt.Name = "tboxEncrypt";
+            this.tboxEncrypt.Size = new System.Drawing.Size(145, 142);
+            this.tboxEncrypt.TabIndex = 6;
+            this.tboxEncrypt.Text = "";
+            // 
+            // tboxKey
+            // 
+            this.tboxKey.Location = new System.Drawing.Point(11, 184);
+            this.tboxKey.Name = "tboxKey";
+            this.tboxKey.Size = new System.Drawing.Size(145, 129);
+            this.tboxKey.TabIndex = 7;
+            this.tboxKey.Text = "";
+            // 
+            // tboxOut
+            // 
+            this.tboxOut.Location = new System.Drawing.Point(285, 69);
+            this.tboxOut.Name = "tboxOut";
+            this.tboxOut.Size = new System.Drawing.Size(214, 183);
+            this.tboxOut.TabIndex = 8;
+            this.tboxOut.Text = "";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(374, 53);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 9;
+            this.lblOutput.Text = "Output";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(188, 147);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 10;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 342);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.tboxOut);
+            this.Controls.Add(this.tboxKey);
+            this.Controls.Add(this.tboxEncrypt);
+            this.Controls.Add(this.lblKey);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Form1";
+            this.Text = "Vernam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.RichTextBox tboxEncrypt;
+        private System.Windows.Forms.RichTextBox tboxKey;
+        private System.Windows.Forms.RichTextBox tboxOut;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
---
+++ Verman/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            encrypt(tboxEncrypt.Text);
+        }
+
+        private void encrypt(string input)
+        {
+            List<string> binary = new List<string>();
+            List<string> key;
+            List<string> xor;
+            byte[] ascii = Encoding.ASCII.GetBytes(input);
+
+            for (int i = 0; i < ascii.Length; i++)
+            {
+                binary.Add(toBinary(ascii[i],7));
+            }
+
+            key = generateKey(binary.Count);
+
+            tboxKey.Text = listToString(key);
+
+            xor = xorLists(binary, key);
+            
+            
+
+            tboxOut.Text = listToString(xor);
+
+        }
+
+        private void decrypt(string input, string key)
+        {
+            List<string> inputList = stringToList(input);
+            List<string> inputKey = stringToList(key);
+            List<string> xor = xorLists(inputList, inputKey);
+            string output = string.Empty;
+
+            output = listBinToString(xor);
+
+            tboxOut.Text = output;
+        }
+
+        /*
+        private string keyToString(List<string> key)
+        {
+
+
+
+            return 
+        }
+        */
+
+        private string binToString(string bin)
+        {
+            string output = string.Empty;
+
+            byte[] bytes = new byte[1];
+            bytes[0] = Convert.ToByte(bin, 2);
+            output += (Encoding.ASCII.GetString(bytes));
+
+            return output;
+        }
+
+
+        private string listBinToString(List<string> bin)
+        {
+
+            string output = string.Empty;
+
+            foreach(string binary in bin)
+            {
+                byte[] bytes = new byte[1];
+                bytes[0] = Convert.ToByte(binary, 2);
+                output += (Encoding.ASCII.GetString(bytes));
+            }
+
+            return output;
+        }
+        
+        private List<string> stringToList(string str)
+        {
+            List<string> output = new List<string>();
+
+            for(int i = 0; i < str.Length; i+= 7)
+            {
+                string sub = str.Substring(i, 7);
+                output.Add(sub);
+            }
+
+            return output;
+        }
+
+        private string listToString(List<string> list)
+        {
+            string output = string.Empty;
+
+            foreach (string str in list)
+            {
+                output += str;
+            }
+
+            return output;
+        }
+
+        private List<string> xorLists(List<string> binary, List<string> key)
+        {
+
+            List<string> xor = new List<string>();
+
+            for(int i = 0; i < binary.Count; i++)
+            {
+                string curBinary = binary[i];
+                string curKey = key[i];
+                string curXor = string.Empty;
+
+                for (int j = 0; j < curBinary.Length; j++)
+                {
+                    char binBit = curBinary[j];
+                    char binKey = curKey[j];
+
+                    if (binBit != binKey)
+                    {
+                        curXor += "1";
+                    }
+                    else
+                    {
+                        curXor += "0";
+                    }
+                }
+
+                xor.Add(curXor);
+            }
+            
+            return xor;
+        }
+
+        private List<string> generateKey(int maxlength)
+        {
+            Random rnd = new Random();
+            List<string> ascii = new List<string>();
+
+            for (int i = 0; i < maxlength; i++)
+            {
+                string randomAscii = toBinary(rnd.Next(97, 123));
+
+                ascii.Add(randomAscii);
+            }
+
+            ascii.Reverse();
+
+            return ascii;
+
+        }
+        
+        private string toBinary(int decimalNumber)
+        {
+            int remainder;
+            string binaryStr = string.Empty;
+            List<int> binary = new List<int>();
+
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                binary.Add(remainder);
+                decimalNumber = decimalNumber / 2;
+            }
+
+            binary.Reverse();
+
+            foreach(int i in binary)
+            {
+                binaryStr = binaryStr + Convert.ToString(i);
+            }
+
+            return binaryStr;
+        }
+
+        private string toBinary(int decimalNumber, int paddingLength)
+        {
+            int remainder;
+            string binaryStr = string.Empty;
+            List<int> binary = new List<int>();
+
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                binary.Add(remainder);
+                decimalNumber = decimalNumber / 2;
+            }
+
+            if(binary.Count != paddingLength)
+            {
+                int padding = paddingLength - binary.Count;
+                for(int i = 0; i < padding; i++)
+                {
+                    binary.Add(0);
+                }
+            }
+
+            binary.Reverse();
+
+            foreach (int i in binary)
+            {
+                binaryStr = binaryStr + Convert.ToString(i);
+            }
+
+            return binaryStr;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            decrypt(tboxEncrypt.Text, tboxKey.Text);
+        }
+    }
+}
+
